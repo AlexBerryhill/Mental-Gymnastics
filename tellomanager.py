4323de8ea227ff165ab5e1ae0da6b61b6ea6a7a8
Kyle Murdock <166570055+kylemur@users.noreply.github.com>
2025-03-01 06:22:56 -0700

    New Commit

--- tellomanager.py
+++ tellomanager.py
@@ -86,7 +86,7 @@ class TelloManager:
         """
         infos = self.get_subnets()
         ips = SubnetInfo.flatten([info.get_ips() for info in infos])
-        ips = list(filter(lambda ip: ip.startswith('192.168.3.'), ips))
+        ips = list(filter(lambda ip: ip.startswith('192.168.2.'), ips))
         return ips
 
     def get_subnets(self):
